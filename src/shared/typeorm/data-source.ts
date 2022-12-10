5a580e2dc263ea6efdae996dac26410d0f6a4cb3
Robson Trasel <robson.trasel@gmail.com>
2022-12-09 22:49:52 -0300

    fix: changed path for recognize migrations folder

--- src/shared/typeorm/data-source.ts
+++ src/shared/typeorm/data-source.ts
@@ -1,17 +1,14 @@
 import { DataSource } from "typeorm";
 
-export const AppDataSource = new DataSource({
+export const AppDataSource = new DataSource ({
   type: 'postgres',
   host: 'localhost',
   port: 5432,
   username: "postgres",
   password: "docker",
   database: "postgres",
-  entities: []
+  migrations: ["./src/shared/typeorm/migrations/*.ts"],
+  
 })
 
-AppDataSource.initialize().then(() => {
-  console.log('[Database] Database is running')
-}).catch(err => {
-  console.log('[Database] Error during Data Source initialization', err)
-})
+

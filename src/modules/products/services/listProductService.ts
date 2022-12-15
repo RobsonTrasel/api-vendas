ae405d04ecee893e029c7a686184916e86ef8b8d
Robson Trasel <robson.trasel@gmail.com>
2022-12-14 23:15:26 -0300

    fix: delete app error on list products

--- src/modules/products/services/listProductService.ts
+++ src/modules/products/services/listProductService.ts
@@ -1,7 +1,6 @@
 import { getCustomRepository } from "typeorm"
 import { ProductRepository } from "../typeorm/repositories/productsReposity"
 import { Product } from "../typeorm/entities/product";
-import AppError from '../../../shared/errors/appError';
 
 
 export class ListProductService {

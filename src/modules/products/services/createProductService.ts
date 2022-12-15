e06ea646555866d60dc2c84f0ca03c868dfe81f3
Robson Trasel <robson.trasel@gmail.com>
2022-12-14 23:07:07 -0300

    feat: created service for create products

---
+++ src/modules/products/services/createProductService.ts
@@ -0,0 +1,30 @@
+import { getCustomRepository } from "typeorm"
+import { ProductRepository } from "../typeorm/repositories/productsReposity"
+import { Product } from "../typeorm/entities/product";
+import AppError from '../../../shared/errors/appError';
+
+
+interface IRequest {
+  name: string
+  price: number
+  quantity: number
+}
+
+export class CreateProductService {
+  public async execute({name, price, quantity}: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository)
+    const productExists = await productRepository.findByName(name)
+
+    if(productExists) {
+      throw new AppError('There is already one product with this name')
+    }
+
+    const product = productRepository.create({
+      name, price, quantity
+    })
+
+    await productRepository.save(product)
+
+    return product
+  }
+}
